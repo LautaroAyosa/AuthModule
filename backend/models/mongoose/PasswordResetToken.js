df9ae7ecbdeea376ec0f2ff48ca294fcefe1bdd7
lautaroayosa <lautaroayosa@gmail.com>
2025-03-12 21:36:03 -0300

    Removed backend and frontend to use submodules

--- backend/models/mongoose/PasswordResetToken.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const passwordResetTokenSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
-  token: String,
-  expiresAt: Date
-});
-
-module.exports = mongoose.model('passwordResetToken', passwordResetTokenSchema);
